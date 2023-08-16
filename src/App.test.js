1f1a4dacaf869f9a0dffef1f43786102a7071a9e
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-08-16 22:39:21 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import axios from 'axios'; // You might want to mock axios
+import {ExpenseForm} from './ExpenseForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock axios to prevent actual API calls during testing
+jest.mock('axios');
+
+describe('ExpenseForm component', () => {
+  // Mock axios post method
+  axios.post.mockResolvedValue({ data: {} });
+
+  it('renders correctly', () => {
+    render(<ExpenseForm />);
+    
+    expect(screen.getByText('Expense Tracker')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Money Spent')).toBeInTheDocument();
+    // Add more assertions as needed
+  });
+
+  it('handles adding an expense', async () => {
+    render(<ExpenseForm />);
+    
+    // Mock user input
+    fireEvent.change(screen.getByTestId('money-spent-input'), { target: { value: '50' } });
+    // Simulate form submission
+    fireEvent.click(screen.getByTestId('add-expense-button'));
+
+    // Wait for API call to resolve
+    await Promise.resolve();
+
+    // Assert that the axios.post function was called
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    // You can add more assertions here based on your UI changes after adding an expense
+  });
+
+  // Add more test cases for other interactions and features
+
 });
